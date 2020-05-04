27ea2b731493b61468889e3d6b51dbdfad30cde6
nxn1710 <62548900+nxn1710@users.noreply.github.com>
2020-05-04 20:45:59 +0700

    nxn

--- index.js
+++ index.js
@@ -51,44 +51,22 @@ bot.on('message', async message => {
         case 'klmda':
             message.channel.send("https://tenor.com/view/huan-rose-talk-selfie-shout-gif-16605201");
             break;
-        case 'test':
-            const exampleEmbed2 = new MessageEmbed()
-                .setColor('#f62500')
-                .setTitle('Lịch Thi Chuyên Ngành 2')
-                .setDescription('-----------------------------------------------')
-                .addFields(
-                    { name: 'DBI', value: '\tTime: 15h15 - Date: 20/04/2020' },
-                )
-                .addFields(
-                    { name: 'PRO192', value: '\tTime: 13h30 - Date: 21/04/2020' },
-                )
-                .addFields(
-                    { name: 'MAD101', value: '\tTime: 15h15 - Date: 21/04/2020' },
-                )
-                .addFields(
-                    { name: 'WED201c', value: '\tTime: 7h30 - Date: 04/05/2020' },
-                )
-                .setTimestamp()
-                .setFooter('Good Luck', 'https://stickershop.line-scdn.net/stickershop/v1/product/1298618/LINEStorePC/main.png;compress=true');
-
-            message.channel.send(exampleEmbed2);
-            break;
         case 'lichthi':
-            const exampleEmbed3 = new MessageEmbed()
-                .setColor('#f62500')
-                .setURL('http://fap.fpt.edu.vn/temp/Import/trangntx2/7a790795-c7c2-4d73-adc9-8bdaab923925/The%20official%20exam%20schedule%20-%20L%E1%BB%8Bch%20thi%20ch%C3%ADnh%20th%E1%BB%A9c.pdf')
-                .setTitle('Lịch Thi Chuyên Ngành 2')
-                .setAuthor('Nxn1710', 'https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/91056560_2719907111629224_8061680943468904448_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=fDwnKX0OCvAAX97o5gg&_nc_ht=scontent.fdad1-1.fna&oh=1ec5daee2074f635c9443a42c90c7666&oe=5EBCB61B', 'https://fb.com/nxn1710')
-                .setDescription('-----------------------------------------------')
-                .setThumbnail('https://i.pinimg.com/originals/f3/2d/d2/f32dd26a74a9277a3963060500c7b5a1.png')
-                .addFields(
-                    { name: ':book: Subject', value: 'WED201c', inline: true },
-                    { name: ':clock9: Time', value: '13h00', inline: true },
-                    { name: ':calendar_spiral: Date', value: '04/05/2020', inline: true },
-                )
-                .setTimestamp()
-                .setFooter('Good Luck', 'https://stickershop.line-scdn.net/stickershop/v1/product/1298618/LINEStorePC/main.png;compress=true');
-            message.channel.send(exampleEmbed3);
+            // const exampleEmbed3 = new MessageEmbed()
+            //     .setColor('#f62500')
+            //     .setURL('http://fap.fpt.edu.vn/temp/Import/trangntx2/7a790795-c7c2-4d73-adc9-8bdaab923925/The%20official%20exam%20schedule%20-%20L%E1%BB%8Bch%20thi%20ch%C3%ADnh%20th%E1%BB%A9c.pdf')
+            //     .setTitle('Lịch Thi Chuyên Ngành 2')
+            //     .setAuthor('Nxn1710', 'https://scontent.fdad1-1.fna.fbcdn.net/v/t1.0-9/91056560_2719907111629224_8061680943468904448_n.png?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=fDwnKX0OCvAAX97o5gg&_nc_ht=scontent.fdad1-1.fna&oh=1ec5daee2074f635c9443a42c90c7666&oe=5EBCB61B', 'https://fb.com/nxn1710')
+            //     .setDescription('-----------------------------------------------')
+            //     .setThumbnail('https://i.pinimg.com/originals/f3/2d/d2/f32dd26a74a9277a3963060500c7b5a1.png')
+            //     .addFields(
+            //         { name: ':book: Subject', value: 'WED201c', inline: true },
+            //         { name: ':clock9: Time', value: '13h00', inline: true },
+            //         { name: ':calendar_spiral: Date', value: '04/05/2020', inline: true },
+            //     )
+            //     .setTimestamp()
+            //     .setFooter('Good Luck', 'https://stickershop.line-scdn.net/stickershop/v1/product/1298618/LINEStorePC/main.png;compress=true');
+            message.channel.send("Thi cái db");
             break;
         case 'retake':
             const exampleEmbed4 = new MessageEmbed()
